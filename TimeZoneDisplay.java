c328c9673862ae6bb9a0727c8efd470dcb495e1c
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-08 10:49:46 +0530

    Displaying date and time in different timezones

---
+++ TimeZoneDisplay.java
@@ -0,0 +1,14 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class TimeZoneDisplay {
+    public static void main(String[] args) {
+        String[] timeZones = {"GMT", "Asia/Kolkata", "America/Los_Angeles"};
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+        for (String zone : timeZones) {
+            ZonedDateTime now = ZonedDateTime.now(ZoneId.of(zone));
+            System.out.println("Current time in " + zone + ": " + now.format(formatter));
+        }
+    }
+}
